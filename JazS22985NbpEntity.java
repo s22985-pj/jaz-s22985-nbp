37b76dd22fd7e6d608e9ca5ce317e802a970c656
s22985-pj <s22985@pjwstk.edu.pl>
2022-07-03 17:19:11 +0200

    commit 2

--- JazS22985NbpEntity.java
+++ JazS22985NbpEntity.java
@@ -1,7 +1,66 @@
 package com.example.jazs22985nbp;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Entity
 public class JazS22985NbpEntity {
+    JazS22985NbpEntity(){}
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String waluta;
+    private String startDate;
+    private String endDate;
+    private String code;
+
+
+    JazS22985NbpEntity(String code, String waluta, String startDate, String endDate){
+    this.waluta=waluta;
+    this.code=code;
+    this.startDate=startDate;
+    this.endDate=endDate;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getWaluta() {
+        return waluta;
+    }
+
+    public void setWaluta(String waluta) {
+        this.waluta = waluta;
+    }
+
+    public String getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(String startDate) {
+        this.startDate = startDate;
+    }
+
+    public String getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(String endDate) {
+        this.endDate = endDate;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
 }
