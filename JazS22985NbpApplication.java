b31dfbc859f0831453da7e518c697e5a5e1c849a
s22985-pj <s22985@pjwstk.edu.pl>
2022-07-03 17:00:25 +0200

    Initial commit

---
+++ JazS22985NbpApplication.java
@@ -0,0 +1,13 @@
+package com.example.jazs22985nbp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JazS22985NbpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JazS22985NbpApplication.class, args);
+	}
+
+}
