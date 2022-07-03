35df987282f180dd4ee730a2f1b2c6f6ed9f4316
s22985-pj <s22985@pjwstk.edu.pl>
2022-07-03 17:06:52 +0200

    commit

--- JazS22985NbpApplication.java
+++ JazS22985NbpApplication.java
@@ -2,9 +2,15 @@ package com.example.jazs22985nbp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class JazS22985NbpApplication {
+	@Bean
+	public RestTemplate restTemplate(){
+		return new RestTemplate();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(JazS22985NbpApplication.class, args);
---
+++ JazS22985NbpEntity.java
@@ -0,0 +1,7 @@
+package com.example.jazs22985nbp;
+
+import javax.persistence.Entity;
+
+@Entity
+public class JazS22985NbpEntity {
+}
